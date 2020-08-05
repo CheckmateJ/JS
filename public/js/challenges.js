08e61810e19028cb61a5732c0825bb3dc698fb67
Michal <matej50@gmail.com>
2020-08-05 13:36:18 +0200

    challenge 2

--- public/js/challenges.js
+++ public/js/challenges.js
@@ -1,4 +1,6 @@
 
+// CHALLENGE 1
+
 var massJohn = 95; //kg
 var heightJohn = 1.7; // meters
 
@@ -23,4 +25,35 @@ if(bmiJohn > bmiMark){
     console.log('John has higher Bmi than Mark')
 }else{
     console.log('Mark has higher bmi than John');
+}
+
+
+// CHALLENGE 2
+
+var johnScores = [819,120,103];
+var ScoresForJohn = 0;
+
+
+for (i = 0; i < johnScores.length; i++){
+    ScoresForJohn += johnScores[i]
+}
+
+var averageScoresForJohn = ScoresForJohn / 3
+console.log('John average scores = ' + averageScoresForJohn);
+
+var markScores = [116,94,123];
+var ScoresForMark = 0;
+
+
+for (i = 0; i < markScores.length; i++){
+    ScoresForMark += markScores[i]
+}
+
+var averageScoresForMark = ScoresForMark / 3
+console.log('Mark average scores = ' + averageScoresForMark)
+
+if(averageScoresForMark > averageScoresForJohn){
+    console.log('Mark\'s team win ')
+}else{
+    console.log('John\'s teams win')
 }
