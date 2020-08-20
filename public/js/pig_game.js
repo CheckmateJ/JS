d90092839589c65694594415f2501194bc956f8b
Michal <matej50@gmail.com>
2020-08-20 17:23:53 +0200

    created EventListener and dice

--- public/js/pig_game.js
+++ public/js/pig_game.js
@@ -3,6 +3,17 @@ var scores, roundScore, activePlayer, dice;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1 ;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+
+document.querySelector('.btn-roll-dice').addEventListener('click', function(){
+    // Random number
+
+    dice = Math.floor(Math.random() * 6) + 1 ;
+
+    // Display the result
+
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+
+
+});
