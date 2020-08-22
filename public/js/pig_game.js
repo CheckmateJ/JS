c1e50f29eaee2eed72ecc7d05c07a1a109c7955c
Michal <matej50@gmail.com>
2020-08-22 04:16:51 +0200

    change view continuing

--- public/js/pig_game.js
+++ public/js/pig_game.js
@@ -5,15 +5,24 @@ roundScore = 0;
 activePlayer = 0;
 
 
+document.querySelector('.dice').style.display = 'none';
 
+
+// if click new game - start game 
+if(document.querySelector('.btn-new-game').addEventListener('click', function() {
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+
+// start to count scores
 document.querySelector('.btn-roll-dice').addEventListener('click', function(){
+    
     // Random number
-
     dice = Math.floor(Math.random() * 6) + 1 ;
 
     // Display the result
+    document.querySelector('#current-scores-' + activePlayer).textContent = dice;
+    diceDOM.src = "../img/dice-" + dice + ".png";
 
-    document.querySelector('#current-' + activePlayer).textContent = dice;
-
-
-});
+})
+}));
